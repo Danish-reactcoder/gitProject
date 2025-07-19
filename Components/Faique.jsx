13912bbe57f40b2b4cb1e2d499b398bddb60e355
Danish-reactcoder <danish2002.ngp@gmail.com>
2025-07-19 18:28:04 +0530

    commit

---
+++ Components/Faique.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Faique = () => {
+  return (
+    <View>
+      <Text>Faique</Text>
+    </View>
+  )
+}
+
+export default Faique
+
+const styles = StyleSheet.create({})
---
+++ Components/Mustakim.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Mustakim = () => {
+  return (
+    <View>
+      <Text>Mustakim</Text>
+    </View>
+  )
+}
+
+export default Mustakim
+
+const styles = StyleSheet.create({})
